53f772bcf454efb1f376874876a5bfe9c9988b96
M. O. Marmalade <xephyrpanda@yahoo.com>
2020-08-04 14:57:14 -0700

    improved cpu, added customizable parameters and score display

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ end
 --GLOBALS-------------------------------------------------------------------------------------------- 
 local app = renoise.app() 
 local song = nil 
+local tool = renoise.tool()
 
 local vb = renoise.ViewBuilder()
 local window_title = nil
@@ -18,21 +19,138 @@ local window_height = 50
 local window_width = 50
 
 local key_handler_options = { 
-  send_key_repeat = true, 
+  send_key_repeat = false, 
   send_key_release = true 
 } 
 
 local ball = {25,25}
 local paddles = {25,25,0,0}
-local paddlesize = 5
-local direction = {-1,0}
+local paddlesize = 9
+local direction = {1,0}
+local movespeed = 2
 local scores = {0,0}
 local pixelgrid = {}
+local msperframe = 40
+local maxspeed = 4
+
+
+--TIMER FUNC----------------------------------------------------------
+local function timer_func()
+
+  --paddle1
+  paddles[1] = paddles[1] + paddles[3] * movespeed
+  if paddles[1] - (paddlesize + 1)/2 < 1 then paddles[1] = (paddlesize + 1)/2
+  elseif paddles[1] + (paddlesize + 1)/2 > window_height then paddles[1] = window_height - (paddlesize - 1)/2
+  end
+  
+  if paddles[3] ~= 0 then    
+    for i = 1, movespeed do
+      pixelgrid[2][paddles[1] - (paddlesize + 1 + (i-1)*2)/2 * paddles[3]].bitmap = "Bitmaps/0.bmp"
+    end
+  end
+  
+  --paddle2
+  if paddles[2] < ball[2] then --get direction
+    paddles[4] = 1 
+  elseif paddles[2] > ball[2] then 
+    paddles[4] = -1 
+  elseif paddles[2] == ball[2] then
+    paddles[4] = 0
+  end
+  
+  paddles[2] = paddles[2] + paddles[4]*movespeed  --apply direction to cpu paddle  
+  
+  if paddles[2] - (paddlesize + 1)/2 < 1 then paddles[2] = (paddlesize + 1)/2
+  elseif paddles[2] + (paddlesize + 1)/2 > window_height then paddles[2] = window_height - (paddlesize - 1)/2
+  end
+  
+  if paddles[4] ~= 0 then
+    for i = 1, movespeed do
+      pixelgrid[49][paddles[2] - (paddlesize + 1 + (i-1)*2)/2 * paddles[4]].bitmap = "Bitmaps/0.bmp"
+    end
+  end
+  
+  --ball
+  pixelgrid[ball[1]][ball[2]].bitmap = "Bitmaps/0.bmp" 
+    
+  ball[1] = ball[1] + direction[1]
+  ball[2] = ball[2] + direction[2]
+  
+  if ball[2] < 1 then ball[2] = 1
+  elseif ball[2] > window_height then ball[2] = window_height
+  end
+  
+  if ball[1] == 3 then
+    if ball[2] > paddles[1] - (paddlesize + 1)/2 and ball[2] < paddles[1] + (paddlesize + 1)/2 then
+      direction[1] = -direction[1]
+      
+      if ball[2] > paddles[1] then
+        direction[2] = direction[2] + 1
+      elseif ball[2] < paddles[1] then
+        direction[2] = direction[2] - 1
+      end
+      
+      if direction[2] > maxspeed then direction[2] = maxspeed
+      elseif direction[2] < -maxspeed then direction[2] = -maxspeed
+      end
+      
+    end
+  elseif ball[1] == 48 then
+    if ball[2] > paddles[2] - (paddlesize + 1)/2 and ball[2] < paddles[2] + (paddlesize + 1)/2 then
+      direction[1] = -direction[1]
+      
+      if ball[2] > paddles[2] then
+        direction[2] = direction[2] + 1
+      elseif ball[2] < paddles[2] then
+        direction[2] = direction[2] - 1     
+      end
+      
+      if direction[2] > maxspeed then direction[2] = maxspeed
+      elseif direction[2] < -maxspeed then direction[2] = -maxspeed
+      end
+      
+    end
+  elseif ball[1] == 0 then
+    scores[2] = scores[2] + 1
+    vb.views.scoretext.text = "\nSCORE\n" .. ("%i:%i"):format(scores[1],scores[2])
+    ball[1] = window_width/2
+    ball[2] = window_height/2
+    direction[1] = -direction[1]
+    direction[2] = 0
+  elseif ball[1] == 51 then
+    scores[1] = scores[1] + 1
+    vb.views.scoretext.text = "\nSCORE\n" .. ("%i:%i"):format(scores[1],scores[2])
+    ball[1] = window_width/2
+    ball[2] = window_height/2
+    direction[1] = -direction[1]
+    direction[2] = 0
+  end
+  
+  if ball[2] == 50 or ball[2] == 1 then
+    direction[2] = -direction[2]
+  end
+  
+  
+  --drawing screen
+  pixelgrid[ball[1]][ball[2]].bitmap = "Bitmaps/1.bmp"
+  
+  local xcoord = 2
+  for p = 1, 2 do
+    for i = 1, (paddlesize + 1)/2 do  
+      if i == 1 then pixelgrid[xcoord][paddles[p]].bitmap = "Bitmaps/1.bmp"
+      else
+        pixelgrid[xcoord][paddles[p] - (i - 1)].bitmap = "Bitmaps/1.bmp"
+        pixelgrid[xcoord][paddles[p] + (i - 1)].bitmap = "Bitmaps/1.bmp"
+      end
+    end
+    xcoord = 49
+  end
+end
 
 --CREATE PONG WINDOW----------------------------------------------------------------------------- 
 function create_pong_window()
 
-  window_title = "Pong"
+  window_title = "| Pong |"
 
   -- create the main content column, but don't add any views yet:
   window_content = vb:row {
@@ -50,7 +168,6 @@ function create_pong_window()
       
       --fill the row with 50 pixels
       pixelgrid[x][y] = vb:bitmap {
-        --id = ("pixel%ix%i"):format(x,y),
         bitmap = "Bitmaps/0.bmp"
       }
     
@@ -64,6 +181,63 @@ function create_pong_window()
     window_content:add_child(row)  
   
   end  
+  
+  local newcolumn = vb:column {
+    vb:button {
+      id = "start_stop",
+      width = "100%",
+      text = "START",
+      notifier = function(t)
+        if vb.views.start_stop.text == "STOP" then
+          tool:remove_timer(timer_func)
+          vb.views.start_stop.text = "START"
+        elseif vb.views.start_stop.text == "START" then        
+          tool:add_timer(timer_func, msperframe)
+          vb.views.start_stop.text = "STOP"
+        end    
+      end
+    },
+    vb:text {
+      text = "Paddle Size"
+    },
+    vb:popup {
+      value = 5,
+      items = {"1","3","5","7","9","11"},
+      notifier = function(value)
+        paddlesize = value + value-1
+      end    
+    },
+    vb:text {
+      text = "Max Ball Speed"
+    },
+    vb:popup {
+      value = 4,
+      items = {"1","2","3","4","5","6"},
+      notifier = function(value)
+        maxspeed = value
+      end    
+    },
+    vb:text {
+      text = "Paddle Speed"
+    },
+    vb:popup {
+      value = 2,
+      items = {"1","2","3","4","5"},
+      notifier = function(value)
+        movespeed = value
+      end    
+    },    
+    vb:text {
+      font = "big",
+      id = "scoretext",
+      text = "\nSCORE\n" .. ("%i:%i"):format(scores[1],scores[2])
+    }
+    
+  }
+  
+  window_content:add_child(newcolumn)
+  
+  
   
 end
 
@@ -82,6 +256,18 @@ local function key_handler(dialog, key)
   
     end
     
+    if key.name == "space" then
+    
+      if vb.views.start_stop.text == "STOP" then
+        tool:remove_timer(timer_func)
+        vb.views.start_stop.text = "START"
+      elseif vb.views.start_stop.text == "START" then        
+        tool:add_timer(timer_func, msperframe)
+        vb.views.start_stop.text = "STOP"
+      end   
+    
+    end
+    
   elseif key.state == "released" then
     
     paddles[3] = 0
@@ -98,69 +284,15 @@ local function show_pong_window()
   end
 end
 
---TIMER FUNC----------------------------------------------------------
-local function timer_func()
-
-  pixelgrid[ball[1]][ball[2]].bitmap = "Bitmaps/0.bmp" 
-  
-  
-  paddles[1] = paddles[1] + paddles[3]
-  if paddles[1] - (paddlesize - 1)/2 == 0 then paddles[1] = paddles[1] + 1
-  elseif paddles[1] + (paddlesize - 1)/2 == window_height + 1 then paddles[1] = paddles[1] - 1
-  end
-  
-  if paddles[3] ~= 0 then
-    pixelgrid[2][paddles[1] - (paddlesize + 1)/2 * paddles[3]].bitmap = "Bitmaps/0.bmp"
-  end
-  
-  ball[1] = ball[1] + direction[1]
-  
-  if ball[1] == 3 then
-    if ball[2] > paddles[1] - (paddlesize + 1)/2 and ball[2] < paddles[1] + (paddlesize + 1)/2 then
-      direction[1] = -direction[1]
-    end
-  elseif ball[1] == 48 then
-    if ball[2] > paddles[2] - (paddlesize + 1)/2 and ball[2] < paddles[2] + (paddlesize + 1)/2 then
-      direction[1] = -direction[1]
-    end
-  elseif ball[1] == 0 then
-    scores[1] = scores[1] + 1
-    ball[1] = window_width/2
-    ball[2] = window_height/2
-    direction[1] = -direction[1]
-  elseif ball[1] == 51 then
-    scores[2] = scores[2] + 1
-    ball[1] = window_width/2
-    ball[2] = window_height/2
-    direction[1] = -direction[1]
-  end
-  
-
-  pixelgrid[ball[1]][ball[2]].bitmap = "Bitmaps/1.bmp"
-  
-  local xcoord = 2
-  for p = 1, 2 do
-    for i = 1, (paddlesize + 1)/2 do  
-      if i == 1 then pixelgrid[xcoord][paddles[p]].bitmap = "Bitmaps/1.bmp"
-      else
-        pixelgrid[xcoord][paddles[p] - (i - 1)].bitmap = "Bitmaps/1.bmp"
-        pixelgrid[xcoord][paddles[p] + (i - 1)].bitmap = "Bitmaps/1.bmp"
-      end
-    end
-    xcoord = 49
-  end
-end
-
 --MAIN FUNCTION-------------------------------------------------------------------------------------- 
 local function main_function()
   if not pong_window_obj then create_pong_window() end
   show_pong_window()
-  renoise.tool():add_timer(timer_func, 40)
 end
 
 --MENU/HOTKEY ENTRIES-------------------------------------------------------------------------------- 
 
-renoise.tool():add_menu_entry { 
+tool:add_menu_entry { 
   name = "Main Menu:Tools:Pong...", 
   invoke = function() main_function() end 
 }
